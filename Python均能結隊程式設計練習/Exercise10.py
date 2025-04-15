15b1fdf9c5804049da8b3f3c32e005c247946c59
Jacky <jackykang2005@gmail.com>
2025-04-15 14:03:47 +0800

    更改Exercise10

--- Python均能結隊程式設計練習/Exercise10.py
+++ Python均能結隊程式設計練習/Exercise10.py
@@ -25,5 +25,5 @@ main()
 
 #說明執行結果的原因:
 #def main()這段使用 try-except 結構包住了對 f() 的呼叫，以及一個在函式呼叫後的 print()。
-#def f(): 1.s = "abc" 是個字串，長度為 3，索引為 0, 1, 2.print(s[5]) 嘗試存取第 6 個字元（index 為 5），這會觸發 IndexError。 3.該錯誤在 f() 函式內的 except IndexError: 中被捕捉，並印出：Index out of bound\
-#因為 f() 自己已經 妥善處理了這個錯誤，所以這個例外 不會再傳回到 main() 的 except 區塊中。程式繼續執行 f() 呼叫之後的 print("After the function call")，所以會接著印出：After the function call
+#def f(): 1.s = "abc" 是個字串，長度為 3，索引為 0, 1, 2.print(s[5]) 嘗試存取第 6 個字元（index 為 5），這會觸發 IndexError。 3.該錯誤在 f() 函式內的 except IndexError: 中被捕捉，並印出："Index out of bound"
+#因為 f() 自己已經 妥善處理了這個錯誤，所以這個例外 不會再傳回到 main() 的 except 區塊中。程式繼續執行 f() 呼叫之後的 print("After the function call")，所以會接著印出："After the function call"
